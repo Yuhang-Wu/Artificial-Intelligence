8208cd24de7e395ab9f6376f6cf63f80df91de46
Yuhang-Wu <yuhang0803@qq.com>
2017-05-05 15:28:52 +1200

    Update JointProbability.py

--- JointProbability.py
+++ JointProbability.py
@@ -18,6 +18,44 @@ def joint_prob(network, assignment):
             
     return probability
 
+""" Write a function query(network, query_var, evidence) that given a belief network, 
+    the name of a variable in the network, and some evidence, returns the posterior distribution of query_var. 
+    The parameter network is a belief network whose data structure was described earlier. 
+    The parameter query_var is the name of the variable we are interested in and is of type string. 
+    The parameter evidence is a dictionary whose elements are assignments to some variables in the network; 
+    the keys are the name of the variables and the values are Boolean.
+    
+    The function must return a pair of real numbers where 
+    the first element is the probability of query_var being false given the evidence and 
+    the second element is the probability of query_var being true given the evidence.
+
+"""
+
+def query(network, query_var, evidence):
+    
+    answer = {}
+    selected = {}
+    assignment = evidence.copy() #or can just use: assignment = evidence
+    query_val = [True, False]
+    hidden_vars = network.keys() - evidence.keys() - {query_var} #hint   
+    
+    for state in query_val:
+        sum_hidden = 0
+        
+        for values in itertools.product((True, False), repeat=len(hidden_vars)):
+            hidden_assignments = {var:val for var,val in zip(hidden_vars, values)} #hint
+            
+            assignment[query_var] = state
+            assignment.update(hidden_assignments)
+            
+            sum_hidden+= joint_prob(network, assignment)
+        
+        selected[state] = sum_hidden
+    
+    for state in query_val:
+        answer[state] = selected[state] / sum(selected.values())
+    return answer
+
 def main():  
     # --------------- --------------- test for joint_prob(network, assignment) --------------- ---------------
     network = {
@@ -120,7 +158,137 @@ def main():
     print("{:.8f}".format(p))      
     
     # --------------- --------------- test for query(network, query_var, evidence) --------------- ---------------
+    network = {
+        'A': {
+            'Parents': [],
+            'CPT': {
+                (): 0.2
+                }},
+    }
     
+    answer = query(network, 'A', {})
+    print("P(A=true)={:.5f}".format(answer[True]))
+    print("P(A=false)={:.5f}".format(answer[False]))    
+    
+    network = {
+    'A': {
+        'Parents': [],
+        'CPT': {
+            (): 0.1
+            }},
+            
+    'B': {
+        'Parents': ['A'],
+        'CPT': {
+            (True,): 0.8,
+            (False,): 0.7,
+            }},
+    }
+ 
+    answer = query(network, 'B', {'A': False})
+    print("P(B=true|A=false) = {:.5f}".format(answer[True]))
+    print("P(B=false|A=false) = {:.5f}".format(answer[False])) 
+    
+    network = {
+        'A': {
+            'Parents': [],
+            'CPT': {
+                (): 0.1
+                }},
+                
+        'B': {
+            'Parents': ['A'],
+            'CPT': {
+                (True,): 0.8,
+                (False,): 0.7,
+                }},
+        }
+     
+    answer = query(network, 'B', {})
+    print("P(B=true) = {:.5f}".format(answer[True]))
+    print("P(B=false) = {:.5f}".format(answer[False]))    
+    
+    
+    network = {
+        'Burglary': {
+            'Parents': [],
+            'CPT': {
+                (): 0.001
+                }},
+                
+        'Earthquake': {
+            'Parents': [],
+            'CPT': {
+                (): 0.002,
+                }},
+        'Alarm': {
+            'Parents': ['Burglary','Earthquake'],
+            'CPT': {
+                (True,True): 0.95,
+                (True,False): 0.94,
+                (False,True): 0.29,
+                (False,False): 0.001,
+                }},
+    
+        'John': {
+            'Parents': ['Alarm'],
+            'CPT': {
+                (True,): 0.9,
+                (False,): 0.05,
+                }},
+    
+        'Mary': {
+            'Parents': ['Alarm'],
+            'CPT': {
+                (True,): 0.7,
+                (False,): 0.01,
+                }},
+        }
+    
+    answer = query(network, 'Burglary', {'John': True, 'Mary': True})
+    print("Probability of a burglary when both\n"
+          "John and Mary have called: {:.3f}".format(answer[True]))     
+    
+    
+    network = {
+        'Burglary': {
+            'Parents': [],
+            'CPT': {
+                (): 0.001
+                }},
+                
+        'Earthquake': {
+            'Parents': [],
+            'CPT': {
+                (): 0.002,
+                }},
+        'Alarm': {
+            'Parents': ['Burglary','Earthquake'],
+            'CPT': {
+                (True,True): 0.95,
+                (True,False): 0.94,
+                (False,True): 0.29,
+                (False,False): 0.001,
+                }},
+    
+        'John': {
+            'Parents': ['Alarm'],
+            'CPT': {
+                (True,): 0.9,
+                (False,): 0.05,
+                }},
+    
+        'Mary': {
+            'Parents': ['Alarm'],
+            'CPT': {
+                (True,): 0.7,
+                (False,): 0.01,
+                }},
+        }
+    
+    answer = query(network, 'John', {'Mary': True})
+    print("Probability of John calling if\n"
+          "Mary has called: {:.5f}".format(answer[True])) 
     
     
 if __name__ == '__main__':
